8c0e9e2a5c842475186ac52fe71d998788c71437
Larry Aasen <larryaasen@gmail.com>
2024-03-18 12:17:43 -0400

    Fixed unit test.

--- test/uvc_test.dart
+++ test/uvc_test.dart
@@ -2,31 +2,25 @@ import 'package:uvc/uvc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    setUp(() {
-      // Additional setup goes here.
-    });
+  test('Uvc library is loaded', () {
+    final uvc = UvcLib();
+    expect(uvc.isLibraryLoaded, isTrue);
 
-    test('Uvc library is loaded', () {
-      final uvc = UvcLib();
-      expect(uvc.isLibraryLoaded, isTrue);
+    expect(uvc.debugLogging, isFalse);
+    expect(uvc.debugLoggingLibUsb, isFalse);
 
-      expect(uvc.debugLogging, isFalse);
-      expect(uvc.debugLoggingLibUsb, isFalse);
+    uvc.dispose();
+    expect(uvc.isLibraryLoaded, isFalse);
+  });
 
-      uvc.dispose();
-      expect(uvc.isLibraryLoaded, isFalse);
-    });
+  test('Uvc devices has a list', () {
+    final uvc = UvcLib();
+    expect(uvc.isLibraryLoaded, isTrue);
 
-    test('Uvc devices has a list', () {
-      final uvc = UvcLib();
-      expect(uvc.isLibraryLoaded, isTrue);
+    expect(uvc.devices.libusb, isNotNull);
+    expect(uvc.devices.get().length, greaterThanOrEqualTo(0));
 
-      expect(uvc.devices.libusb, isNotNull);
-      expect(uvc.devices.get().length, greaterThan(0));
-
-      uvc.dispose();
-      expect(uvc.isLibraryLoaded, isFalse);
-    });
+    uvc.dispose();
+    expect(uvc.isLibraryLoaded, isFalse);
   });
 }
